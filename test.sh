3f6426199a16e64da74b6058250a78a7f539c152
Andrew Stilliard <andrew.stilliard@gmail.com>
2014-06-03 12:18:45 +0100

    Reconfigured the test.sh file to have a simpler way to set the test account

--- test.sh
+++ test.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+site=demo.com
+
 # make directory structure
-mkdir -p /srv/demo.com/{config,public/{htdocs,logs}}
+mkdir -p /srv/$site/{config,public/{htdocs,logs}}
 
 # add a demo index.html file
-echo "<h1>Success symbiosis!</h1>" > /srv/demo.com/public/htdocs/index.html
+echo "<h1>Success symbiosis!</h1>" > /srv/$site/public/htdocs/index.html
 
 # add ip
-echo "127.0.0.1" > /srv/demo.com/config/ip
+echo "127.0.0.1" > /srv/$site/config/ip
 
 # set permissions
-chown admin:admin /srv/demo.com/ -R
-chown admin:www-data /srv/demo.com/public/htdocs -R
-chmod 775 /srv/demo.com/public/htdocs -R
+chown admin:admin /srv/$site/ -R
+chown admin:www-data /srv/$site/public/htdocs -R
+chmod 775 /srv/$site/public/htdocs -R
 
 # run symbiosis configuration scripts to do the rest
 symbiosis-httpd-configure -v
